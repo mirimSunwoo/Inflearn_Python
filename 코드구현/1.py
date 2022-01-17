c8e140405054bc0aecf76c8c83d5b8daf3cd72db
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2022-01-17 23:40:24 +0900

    열세번째 수업 완료 ✔️

---
+++ 코드구현/1.py
@@ -0,0 +1,14 @@
+#k번째 약수 풀이
+import sys
+# sys.stdin = open("input.txt",'rt')
+
+n,k = map(int, input().split()) #각각의 숫자들을 int형으로 바꿈
+cnt = 0
+for i in range(1,n+1):
+    if n%i == 0: #약수찾기
+        cnt += 1
+    if cnt==k:
+        print(i)
+        break
+else: #정상적으로 끝났을때 else문 사용
+    print(-1)
